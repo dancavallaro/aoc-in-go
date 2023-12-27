de2f3a60d2b249e3366f6eefbecb37c9ffef1d69
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2023-12-26 21:06:01 -0500

    Solved day 10 part 2!

--- 2023/10/code.go
+++ 2023/10/code.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	//aoc.Harness(run)
-	util.Run(run, "2023/10/input-user.txt", false)
+	util.Run(run, "2023/10/input-user.txt", true)
 }
 
 type direction struct {
@@ -34,6 +34,10 @@ func dirIJ(deltaI int, deltaJ int) direction {
 	return direction{deltaJ, -deltaI}
 }
 
+func directionBetween(one cell, two cell) direction {
+	return dirIJ(two.i-one.i, two.j-one.j)
+}
+
 type pipe struct {
 	previous, next direction
 }
@@ -52,7 +56,11 @@ func (c cell) String() string {
 	return fmt.Sprintf("[i=%d, j=%d, char=%c]", c.i, c.j, c.char)
 }
 
-var pipes = map[rune]*pipe{
+func (d direction) String() string {
+	return fmt.Sprintf("[%d %d]", d.deltaX, d.deltaY)
+}
+
+var pipes = map[rune]pipe{
 	'|': {dir(0, 1), dir(0, -1)},
 	'-': {dir(-1, 0), dir(1, 0)},
 	'L': {dir(0, 1), dir(1, 0)},
@@ -60,10 +68,11 @@ var pipes = map[rune]*pipe{
 	'7': {dir(-1, 0), dir(0, -1)},
 	'F': {dir(1, 0), dir(0, -1)},
 }
+var pipeDirs = util.InvertMap(pipes)
 
 func connected(one cell, two cell) bool {
 	onePipe, twoPipe := pipes[one.char], pipes[two.char]
-	delta := dirIJ(two.i-one.i, two.j-one.j)
+	delta := directionBetween(one, two)
 	if one.char == 'S' {
 		return twoPipe.previous == delta.negative() || twoPipe.next == delta.negative()
 	} else if two.char == 'S' {
@@ -98,21 +107,85 @@ func nextCell(grid [][]rune, previous cell, start cell) cell {
 	}
 }
 
-func midpointDistance(grid [][]rune, start cell) int {
+func findPipe(grid [][]rune, start cell) []cell {
 	neighbors := findNeighbors(grid, start)
+	pipeCells := []cell{start}
 	previous, current := start, neighbors[0]
 	distance := 1
 	for current.char != 'S' {
+		pipeCells = append(pipeCells, current)
 		next := nextCell(grid, previous, current)
 		previous = current
 		current = next
 		distance++
 	}
-	return distance / 2
+	return pipeCells
+}
+
+func findStartPipe(pipeCells []cell) rune {
+	start, next, last := pipeCells[0], pipeCells[1], pipeCells[len(pipeCells)-1]
+	lastDir, nextDir := directionBetween(start, last), directionBetween(start, next)
+	startPipe := pipeDirs[pipe{lastDir, nextDir}]
+	if startPipe == 0 {
+		startPipe = pipeDirs[pipe{nextDir, lastDir}]
+	}
+	return startPipe
+}
+
+func countCrossings(startI, startJ int, grid [][]rune, cellIsPipe map[cell]bool) int {
+	crossings := 0
+	incomingDirection := 0
+	for j := startJ + 1; j < len(grid[startI]); j++ {
+		char := grid[startI][j]
+		c := cell{startI, j, char}
+
+		if !cellIsPipe[c] {
+			continue
+		}
+
+		if char == '|' {
+			crossings++
+		} else if char != '-' {
+			if char == 'L' {
+				incomingDirection = 1
+			} else if char == 'F' {
+				incomingDirection = -1
+			} else if char == 'J' {
+				if incomingDirection == -1 {
+					crossings++
+					incomingDirection = 0
+				}
+			} else if char == '7' {
+				if incomingDirection == 1 {
+					crossings++
+					incomingDirection = 0
+				}
+			}
+		}
+	}
+	return crossings
+}
+
+func countEnclosedTiles(grid [][]rune, cellIsPipe map[cell]bool) int {
+	enclosedTiles := 0
+	for i, row := range grid {
+		for j := range row {
+			c := cell{i, j, grid[i][j]}
+			if cellIsPipe[c] {
+				continue
+			}
+			crossings := countCrossings(i, j, grid, cellIsPipe)
+
+			if crossings%2 == 1 {
+				enclosedTiles++
+			}
+		}
+	}
+	return enclosedTiles
 }
 
 func run(part2 bool, input string) any {
-	if part2 {
+	if !part2 {
 		return "not implemented"
 	}
 
@@ -121,13 +194,27 @@ func run(part2 bool, input string) any {
 		grid = append(grid, []rune(line))
 	}
 
+	var pipeCells []cell
 	for i, row := range grid {
 		for j, ch := range row {
 			if ch == 'S' {
 				start := cell{i, j, ch}
-				return midpointDistance(grid, start)
+				pipeCells = findPipe(grid, start)
 			}
 		}
 	}
-	panic("how'd we get here?!")
+	if pipeCells == nil {
+		panic("how'd we get here?!")
+	}
+
+	startPipe := findStartPipe(pipeCells)
+	pipeCells[0].char = startPipe
+	grid[pipeCells[0].i][pipeCells[0].j] = startPipe
+
+	cellIsPipe := map[cell]bool{}
+	for _, c := range pipeCells {
+		cellIsPipe[c] = true
+	}
+
+	return countEnclosedTiles(grid, cellIsPipe)
 }
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -45,3 +45,11 @@ func ParseInts(line string) []int {
 	}
 	return ints
 }
+
+func InvertMap[K comparable, V comparable](m map[K]V) map[V]K {
+	inverted := map[V]K{}
+	for k, v := range m {
+		inverted[v] = k
+	}
+	return inverted
+}
