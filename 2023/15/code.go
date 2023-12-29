439009d3661e70404fb4866b1ab1e2de092dfcd8
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2023-12-29 14:45:06 -0500

    Solved day 15 part 2

--- 2023/15/code.go
+++ 2023/15/code.go
@@ -2,12 +2,92 @@ package main
 
 import (
 	"aoc-in-go/pkg/util"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	//aoc.Harness(run)
-	util.Run(run, "2023/15/input-user.txt", false)
+	util.Run(run, "2023/15/input-user.txt", true)
+}
+
+type Lense struct {
+	label       string
+	focalLength int
+}
+
+type Box []Lense
+
+func (b *Box) Insert(l Lense) {
+	for i, lense := range *b {
+		if lense.label == l.label {
+			(*b)[i] = l
+			return
+		}
+	}
+	*b = append(*b, l)
+}
+
+func (b *Box) Remove(label string) {
+	for i, lense := range *b {
+		if lense.label == label {
+			for j := i; j < len(*b)-1; j++ {
+				(*b)[j] = (*b)[j+1]
+			}
+			if len(*b) == 1 {
+				*b = nil
+			} else {
+				*b = (*b)[0 : len(*b)-1]
+			}
+			break
+		}
+	}
+}
+
+type StorageFacility [256]Box
+
+func NewStorageFacility() StorageFacility {
+	return [256]Box{}
+}
+
+func (sf *StorageFacility) Perform(step string) {
+	fmt.Printf("Performing step '%s'\n", step)
+	idx := strings.IndexAny(step, "-=")
+	label := step[0:idx]
+	box := Hash(label)
+
+	if step[idx] == '-' {
+		sf[box].Remove(label)
+	} else if step[idx] == '=' {
+		focalLength, err := strconv.Atoi(step[idx+1:])
+		if err != nil {
+			panic(err)
+		}
+		lense := Lense{label, focalLength}
+		sf[box].Insert(lense)
+	}
+}
+
+func (sf *StorageFacility) FocusingPower() int {
+	power := 0
+	for boxNum, box := range sf {
+		for slotNum, lense := range box {
+			power += (boxNum + 1) * (slotNum + 1) * lense.focalLength
+		}
+	}
+	return power
+}
+
+func (sf StorageFacility) String() string {
+	var sb strings.Builder
+	for i, box := range sf {
+		if len(box) == 0 {
+			continue
+		}
+		sb.WriteString(fmt.Sprintf("Box %d: %v\n", i, box))
+	}
+	return sb.String()
 }
 
 func Hash(input string) int {
@@ -21,16 +101,17 @@ func Hash(input string) int {
 }
 
 func run(part2 bool, input string) any {
-	if part2 {
+	if !part2 {
 		return "not implemented"
 	}
 
 	input = util.Lines(input)[0]
 	steps := strings.Split(input, ",")
-	total := 0
+	sf := NewStorageFacility()
 	for _, step := range steps {
-		total += Hash(step)
+		sf.Perform(step)
+		//fmt.Println(sf)
 	}
 
-	return total
+	return sf.FocusingPower()
 }
